da7348ff0fb6334a71fd1b0f09201ebe2b82cccb
Adam Svensson <adsv18@student.bth.se>
2022-04-03 02:11:18 +0200

    Progress: able to get events

--- src/__main__.py
+++ src/__main__.py
@@ -1,24 +1,103 @@
 
+from calendar import calendar
+from multiprocessing.dummy import active_children
 import sys
-import pdfrw
+import pprint
+import json
+from unittest import result
 
+from google_calendar_setup import *
 
-ANNOT_KEY = '/Annots'
-ANNOT_FIELD_KEY = '/T'
-ANNOT_VAL_KEY = '/V'
-ANNOT_RECT_KEY = '/Rect'
-SUBTYPE_KEY = '/Subtype'
-WIDGET_SUBTYPE_KEY = '/Widget'
+PDF_PATH = "../templates/bth_time_report_template.pdf"
+CALENDAR_PROFILES_PATH = "../profiles/calendar_profiles.json"
 
-pdf_template = "bth_time_report_template.pdf"
+CALENDAR_PROFILE_NAME = "google-teaching-assistant"
 
-template_pdf = pdfrw.PdfReader(pdf_template)
+PP = pprint.PrettyPrinter(indent=4)
 
 def write_data_to_pdf():
     return 0
 
-def read_data_from_calendar():
+def read_calendar_profiles():
+    # Opening JSON file
+    with open(CALENDAR_PROFILES_PATH) as json_file:
+        data = json.load(json_file)
+    
+    return data["calendars_profiles"]
+
+def get_single_profile(profile_name):
+    calendar_profiles = read_calendar_profiles()
+
+    for profile in calendar_profiles:
+        if profile_name in profile:
+            return profile[profile_name]
+        
+    return -1
+
+def get_calendar_id():
+    profile = get_single_profile(CALENDAR_PROFILE_NAME)
+    return profile["calendar_id"]
+
+def get_calendar_item(calendar_id):
+    authenticate_user()
+    service = get_authenticated_user()
+    result = service.calendarList().list().execute()
+
+    for calendar_item in result["items"]:
+        if calendar_item["id"] == calendar_id:
+            return calendar_item
+
+    return -1
+
+def get_activity_key_word(activity_key_word):
+    calendar_profiles = read_calendar_profiles()
+
+    for profile in calendar_profiles:
+        for profile_settings in profile.values():
+            if profile_settings["activity_key_word"] == activity_key_word:
+                return profile_settings["activity_key_word"]
+    
+    return -1
+
+def get_events(calendar_id):
+    authenticate_user()
+    service = get_authenticated_user()
+
+    events = service.events().list(calendarId=calendar_id).execute()
+    return events
+
+def get_calendar_event_names(events, activity_key_word):
+    
+    return_list = []
+    key_word_length = len(activity_key_word)
+
+    for event_entry in events['items']:
+        if activity_key_word == event_entry["summary"][:key_word_length]:
+            return_list.append(event_entry['summary'])
+        
+
+    return return_list, events
+
+def get_event_times(event_name_list):
     return 0
+
+def read_data_from_calendar():
+    
+    calendar_id = get_calendar_id()
+    activity_key_word = get_activity_key_word("TA:")
+    
+    if get_calendar_item(calendar_id) == -1:
+        print("Failed to get calendar_item with calendar_id: ", calendar_id)
+        sys.exit(-1)
+
+    events = get_events(calendar_id)
+    
+    event_name_list = get_calendar_event_names(events, activity_key_word)
+
+    
+    
+
+    
 
 def read_user_profile():
     return 0
@@ -27,13 +106,7 @@ def read_template():
     return 0
 
 def main() -> int:
-    for page in template_pdf.pages:
-        annotations = page[ANNOT_KEY]
-        for annotation in annotations:
-            if annotation[SUBTYPE_KEY] == WIDGET_SUBTYPE_KEY:
-                if annotation[ANNOT_FIELD_KEY]:
-                    key = annotation[ANNOT_FIELD_KEY][1:-1]
-                    print(key)
+    read_data_from_calendar()
 
     return 0
 
--- src/google_calendar_setup.py
+++ src/google_calendar_setup.py
@@ -1,14 +1,22 @@
-from apiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-
 import pickle
 
-scopes = ['https//www.googleapis.com/auth/calendar']
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from os.path import exists
+
+scopes = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def authenticate_user():
-    flow = InstalledAppFlow.from_client_secrets_file('..//credentials//client_secret.json', scopes=scopes)
-    flow.run_console()
-    pickle.dump(credentials, open("..//credentials//token.pkl", "wb"))
+    if exists("../credentials/token.pkl"):
+        print("--------------------------------------------")
+        print("--- User Authenticate Already Performed ----")
+        print("--------------------------------------------")
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file('..//credentials//client_secret.json', scopes=scopes)
+        credentials = flow.run_console()
+        pickle.dump(credentials, open("../credentials/token.pkl", "wb"))
 
 def get_authenticated_user():
-    credentials = pickle.load(open("..//credentials//token.pkl", "rb"))
+    credentials = pickle.load(open("../credentials/token.pkl", "rb"))
+    service = build("calendar", "v3", credentials=credentials)
+    return service
